7252924d1c6eb84de81130de24692ede68ef70e9
alanh5216 <111913523+alanh5216@users.noreply.github.com>
2026-03-30 05:06:07 -0700

    finished move api and created first flight mission

---
+++ TestComponents/test_flight0.py
@@ -0,0 +1,28 @@
+from pymavlink import mavutil
+from test_change_flight_mode import change_flight_mode
+from test_arm_drone import arm_drone
+import test_move
+import test_local_position
+
+if __name__ == "__main__":
+    serial_port = '/dev/serial0'
+    baudrate =  57600
+    source_system = 1
+    source_component = 191
+
+    print("\nConnecting to Pixhawk & Waiting for Heartbeat")
+    master = mavutil.mavlink_connection(serial_port, baud=baudrate, source_system=source_system, source_component=source_component)
+    master.target_system = 1 # Send messages to system 1(drone/vehicle #1)
+    master.target_component = 1 # Send messages to flight controller "autopilot"
+    master.wait_heartbeat()
+    print("Heartbeat Received & Connection Established")
+    print(f"Source System: {master.source_system}, Source Component: {master.source_component}, Target System: {master.target_system}, Target Component: {master.target_component}, Connection Type: {serial_port}, Baudrate: {baudrate}")
+
+    change_flight_mode(master, "GUIDED")
+    arm_drone(master)
+    test_local_position.test_request_local_position_messages(master)
+    test_local_position.test_set_origin_local_position(master)
+    test_local_position.test_update_local_position(master)
+    test_move.test_takeoff(master, 5)
+    test_move.test_move(master, 5, 5, 5)
+    test_move.test_land_current_position(master)
--- TestComponents/test_move.py
+++ TestComponents/test_move.py
@@ -1,14 +1,48 @@
-# This code tests getting the local position and storing those values to the variables in the global variable files,
-# we calculate our custom local positioning and store the value in a global variable. We constatly poll and print the 
-# local position values until we stop the script, this is to ensure that we are able to continuously read the local position
-# and see that the values change as we move the drone around.
-from pymavlink import mavutil
+# This code tests the movement of the drone using custom local positioning, we use the local position messages to calculate our own custom local position 
+# and then we use those values to tell the drone to move to specific coordinates relative to its current position. 
 import global_variables
+from test_local_position import test_update_local_position
 import time
 
 def test_takeoff(master, height):
     print(f"Entered test_takeoff() for Target System: {master.target_system} & Target Component: {master.target_component}")
 
+    # 1. Takeoff to specified height (negative value for NED frame)
+    test_move(master, 0, 0, -height)
 
-def test_move_relative(master, x, y, z):
-    print(f"Entered test_move_relative() for Target System: {master.target_system} & Target Component: {master.target_component}")
+def test_land_current_position(master):
+    print(f"Entered test_land_current_position() for Target System: {master.target_system} & Target Component: {master.target_component}")
+
+    # 1. Land by using land command, this command using latitude and longitude however if set to 0 for both then it will ignore them
+    #    and instead land at current position. This allows the code to work for gps and non-gps implementations. 
+    master.mav.command_long_send(master.target_system, master.target_component, mavutil.mavlink.MAV_CMD_NAV_LAND,
+    0, 0, 0, 0, 0, 0, 0, 0)
+
+def test_move(master, x, y, z):
+    print(f"Entered test_move() for Target System: {master.target_system} & Target Component: {master.target_component}")
+
+    # 1. Tell drone to move to specified position relative to current position using custom origin coordinates reference frame, until within 0.8 meters of target position
+    while(abs(global_variables.current_position_custom_x - x) > 0.8 and abs(global_variables.current_position_custom_y - y) > 0.8 and abs(global_variables.current_position_custom_z - z) > 0.8):
+        test_hold_position(master, global_variables.origin_drone_x + x, global_variables.origin_drone_y + y, global_variables.origin_drone_z + z, 2)
+        test_update_local_position(master)
+
+
+
+def test_goto(master, x, y, z):
+    # 1. Send SET_POSITION_TARGET_LOCAL_NED message to move the drone to the specified position
+    master.mav.set_position_target_local_ned_send(0, master.target_system, master.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, 
+        ( mavutil.mavlink.POSITION_TARGET_TYPEMASK_VX_IGNORE | mavutil.mavlink.POSITION_TARGET_TYPEMASK_VY_IGNORE | mavutil.mavlink.POSITION_TARGET_TYPEMASK_VZ_IGNORE |
+        mavutil.mavlink.POSITION_TARGET_TYPEMASK_AX_IGNORE | mavutil.mavlink.POSITION_TARGET_TYPEMASK_AY_IGNORE | mavutil.mavlink.POSITION_TARGET_TYPEMASK_AZ_IGNORE |
+        mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_IGNORE | mavutil.mavlink.POSITION_TARGET_TYPEMASK_YAW_RATE_IGNORE),
+        x, y, z, # X, Y, Z (NED)
+        0, 0, 0,
+        0, 0, 0,
+        0, 0)
+
+
+def test_hold_position(master, x, y, z, duration):
+    # 1. Continously tell drone to go to specified position for specified duration
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        test_goto(master, x, y, z)
+        time.sleep(0.5)
