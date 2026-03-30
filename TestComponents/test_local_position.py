148e363202e1e1d6eba28edb9840c4dd66e8cd20
alanh5216 <111913523+alanh5216@users.noreply.github.com>
2026-03-30 04:00:24 -0700

    comments

--- TestComponents/test_local_position.py
+++ TestComponents/test_local_position.py
@@ -1,7 +1,6 @@
 # This code tests getting the local position and storing those values to the variables in the global variable files,
-# we calculate our custom local positioning and store the value in a global variable. We constatly poll and print the 
-# local position values until we stop the script, this is to ensure that we are able to continuously read the local position
-# and see that the values change as we move the drone around.
+# we calculate our custom local positioning and store the value in a global variable. This file specifically 
+# compartmentalizes the steps that generally only need to be done once.
 from pymavlink import mavutil
 import global_variables
 import time
