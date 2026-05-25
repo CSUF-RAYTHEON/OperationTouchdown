82ebc0344c3215df0dea4c53cb282232b3106582
alanh5216 <111913523+alanh5216@users.noreply.github.com>
2026-05-24 21:44:22 -0700

    broadcaster now updates shared memory for attitude and parent process cleanup

--- VIOSLAM/test_vio.py
+++ VIOSLAM/test_vio.py
@@ -10,32 +10,38 @@ from TestComponents import test_change_flight_mode
 from pymavlink import mavutil
 
 if __name__ == "__main__":
-    W, H = 640, 400
-    RGB_BYTES = W * H * 3
-    GRAY_BYTES = W * H
-    DEPTH_BYTES = W * H * 2  # 16-bit depth uses 2 bytes per pixel
-    CALIB_BYTES = 3 * 3 * 8 # 3x3 matrix of float64
-    ATTITUDE_BYTES = 24 # 3 float64
-    POSITION_BYTES = 24 # 3 float64 
-
     # 1. Set the multiprocessing start method to 'spawn' this is so that when a new process is started it
     #    doesn't inherit the memory of the parent process, and instead creates its own memory space and also
     #    its own python interpreter. Effectively isolating the processes from each other except for the shared memory.
     mp.set_start_method('spawn', force=True)
-    # 2. Create the shared memory for RGB, gray, depth, and camera calibration matrix
+    
+    W, H = 640, 400
+    RGB_BYTES = W * H * 3
+    GRAY_BYTES = W * H
+    DEPTH_BYTES = W * H * 2  # 16-bit depth uses 2 bytes per pixel
+    CALIB_BYTES = 3 * 3 * 8 # 3x3 matrix of float64 (8 bytes each)
+    YAW_BYTES = 8 # float64 for yaw value
+    COORD_BYTES = 8 # float64 for coordinate value
+
+    # 2. Create the shared memory variables for RGB, gray, depth frames, and camera calibration matrix
     print("Broadcaster tester allocating shared memory...")
     shm_rgb = shared_memory.SharedMemory(create=True, size=RGB_BYTES, name="oak_rgb")
     shm_gray = shared_memory.SharedMemory(create=True, size=GRAY_BYTES, name="oak_gray")
     shm_depth = shared_memory.SharedMemory(create=True, size=DEPTH_BYTES, name="oak_depth")
     shm_calib = shared_memory.SharedMemory(create=True, size=CALIB_BYTES, name="oak_calib")
-    # 3. Create the shared memory for attitude and position 
-    shm_attitude = shared_memory.SharedMemory(create=True, size=ATTITUDE_BYTES, name="attitude")
-    shm_position = shared_memory.SharedMemory(create=True, size=POSITION_BYTES, name="position")
+    shm_yaw = shared_memory.SharedMemory(create=True, size=YAW_BYTES, name="pixhawk_yaw")
+    shm_x_coord = shared_memory.SharedMemory(create=True, size=COORD_BYTES, name="pixhawk_x_coord")
+    shm_y_coord = shared_memory.SharedMemory(create=True, size=COORD_BYTES, name="pixhawk_y_coord")
+    shm_z_coord = shared_memory.SharedMemory(create=True, size=COORD_BYTES, name="pixhawk_z_coord")
 
-    # 4. Create the mutex lock for the camera frame variables, camera calibration variable, attitude variables, and position variables
+    shared_yaw = np.ndarray((1,), dtype=np.float64, buffer=shm_yaw.buf)
+    shared_yaw[0] = 999.0  # VIO script looks for this to know it hasn't updated yet
+
+    # 2. Create the mutex lock for the camera frame variables, camera calibration variable, and uart tx port variable
     camera_frame_mutex = mp.Lock()
     camera_calibration_mutex = mp.Lock()
-    attitude_mutex = mp.Lock()
+    uart_tx_mutex = mp.Lock()
+    yaw_mutex = mp.Lock()
     position_mutex = mp.Lock()
 
     serial_port = '/dev/serial0'
@@ -50,6 +56,11 @@ if __name__ == "__main__":
     master.wait_heartbeat()
     print("Heartbeat Received & Connection Established")
     print(f"Source System: {master.source_system}, Source Component: {master.source_component}, Target System: {master.target_system}, Target Component: {master.target_component}, Connection Type: {serial_port}, Baudrate: {baudrate}")
+
+    initial_yaw_rad = test_getyaw.get_yaw(master)
+    with yaw_mutex:
+        shared_yaw[0] = initial_yaw_rad
+        print(f"Initial yaw set in shared memory: {shared_yaw[0]} radians")
 
     # 3. Define the independent processes
     broadcaster_process = mp.Process(target=camera_broadcaster, args=(camera_frame_mutex, camera_calibration_mutex))
@@ -87,4 +98,12 @@ if __name__ == "__main__":
         shm_depth.unlink()
         shm_calib.close()
         shm_calib.unlink()
+        shm_yaw.close()
+        shm_yaw.unlink()
+        shm_x_coord.close()
+        shm_x_coord.unlink()
+        shm_y_coord.close()
+        shm_y_coord.unlink()
+        shm_z_coord.close()
+        shm_z_coord.unlink()
         print("Integrating vioslam tester processes terminated safely.")
--- VIOSLAM2/broadcaster.py
+++ VIOSLAM2/broadcaster.py
@@ -1,25 +1,36 @@
 import numpy as np
 import depthai as dai
 from multiprocessing import shared_memory
+from controls.connect import connect_UART2
+from controls.attitude import request_attitude_messages
+from controls.attitude import get_attitude
 
-def camera_broadcaster(camera_frame_mutex, camera_calibration_mutex):
+def broadcaster(camera_frame_mutex, camera_calibration_mutex, attitude_mutex):
     W, H = 640, 400
     FPS = 30.0
-
-    # 1. Connect to the three shared memory blocks for RGB, gray, and depth frames
+    # 1. Connect to the shared memory for RGB, gray, and depth
     shm_rgb = shared_memory.SharedMemory(name="oak_rgb")
     shm_gray = shared_memory.SharedMemory(name="oak_gray")
     shm_depth = shared_memory.SharedMemory(name="oak_depth")
     shm_calib = shared_memory.SharedMemory(name="oak_calib")
+    # 2. Connect to shared memory for attitude
+    shm_attitude = shared_memory.SharedMemory(name="attitude")
+    # 3. Create numpy arrays that use the shared memory buffers for RGB, gray, depth, and camera calibration matrix
     shared_rgb = np.ndarray((H, W, 3), dtype=np.uint8, buffer=shm_rgb.buf)
     shared_gray = np.ndarray((H, W), dtype=np.uint8, buffer=shm_gray.buf)
     shared_depth = np.ndarray((H, W), dtype=np.uint16, buffer=shm_depth.buf)
     shared_calib = np.ndarray((3, 3), dtype=np.float64, buffer=shm_calib.buf)
-    print("Broadcaster shared memory connected. Booting camera...")
+    # 4. Create numpy array for the shared memory buffer for attitude
+    shared_attitude = np.ndarray((3,), dtype=np.float64, buffer=shm_attitude.buf)
+    print("Broadcaster shared memory connected")
+    # 5. Connect to the Pixhawk via UART2 and request the ATTITUDE message stream at 50ms intervals
+    master_uart2 = connect_UART2()
+    request_attitude_messages(master_uart2, 50)
 
     with dai.Device() as device:
         with dai.Pipeline(device) as pipeline:
-            # 2. Create the three camera nodes for RGB, left gray, and right gray, and the stereo depth node.
+            print("Starting up camera")
+            # 6. Create the three camera nodes for RGB, left gray, and right gray, and the stereo depth node.
             #    As well as the sync node to synchronize the frames from all three cameras together. Then create
             #    the output queues for the synchronized frames using the camera nodes and the stereo depth node 
             #    as the sources. And lastly start the camera frame pipeline.
@@ -48,30 +59,35 @@ def camera_broadcaster(camera_frame_mutex, camera_calibration_mutex):
 
             sync_q = sync.out.createOutputQueue(maxSize=4, blocking=False)
             pipeline.start()
-            print("Broadcaster camera running. Writing frames...")
-
+            print("Camera running")
+            # 7. Get the camera calibration data and write it to the shared memory
             calib = device.getCalibration()
             K = np.array(calib.getCameraIntrinsics(dai.CameraBoardSocket.CAM_B, W, H), dtype=np.float64)
             with camera_calibration_mutex:
                 np.copyto(shared_calib, K)
-            print("Broadcaster calibration data written to shared memory.")
-
+            print("Camera calibration data written to shared memory.")
+            print("Broadcaster entering main loop to get camera frames and attitude data")
+            
             while pipeline.isRunning():
                 msg_group = sync_q.get()
+                # 8. Get the attitude data from the Pixhawk and store it in the local attitude variable. 
+                attitude = get_attitude(master_uart2)
                 if msg_group is None:
                     continue
-                
-                # 3. Get the RGB, gray, and depth frames from the camera and store them in the the local frame variables.
+                # 9. Get the RGB, gray, and depth frames from the camera and store them in the the local frame variables.
                 rgb_frame = msg_group["rgb"].getCvFrame()
                 gray_frame = msg_group["left"].getCvFrame()
                 depth_frame = msg_group["depth"].getFrame()
-
-                # 4. Force RGB to 3 channels just in case it brings an alpha channel (BGRA)
+                # 10. Force RGB to 3 channels just in case it brings an alpha channel (BGRA)
                 if len(rgb_frame.shape) == 3 and rgb_frame.shape[2] == 4:
                     rgb_frame = rgb_frame[:, :, :3]
-
+                # 11. This section is a critical section as we must aquire the mutex/lock before 
+                #     writing to the shared memory, and release it immediately after.
+                with attitude_mutex:
+                    if attitude is not None:
+                        shared_attitude[:] = attitude
                 try:
-                    # 5. This section is a critical section as we must aquire the mutex/lock before 
+                    # 12. This section is a critical section as we must aquire the mutex/lock before 
                     #    copying from the shared memory, and release it immediately after.
                     with camera_frame_mutex:
                         np.copyto(shared_rgb, rgb_frame)
