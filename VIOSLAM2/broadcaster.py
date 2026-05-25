3e5d223739764b731926362f1f6c93023fc894f9
alanh5216 <111913523+alanh5216@users.noreply.github.com>
2026-05-25 00:45:51 -0700

    testing code for broadcaster.py

--- VIOSLAM2/broadcaster.py
+++ VIOSLAM2/broadcaster.py
@@ -1,5 +1,8 @@
 import numpy as np
 import depthai as dai
+import time
+import cv2
+import multiprocessing as mp
 from multiprocessing import shared_memory
 from controls.connect import connect_UART2
 from controls.attitude import request_attitude_messages
@@ -101,3 +104,108 @@ def broadcaster(camera_frame_mutex, camera_calibration_mutex, attitude_mutex):
                     print(f"Camera sent Depth shape: {depth_frame.shape} | Expected: {shared_depth.shape}")
                     print("Shutting down broadcaster...\n")
                     break
+
+def test_viewer(camera_frame_mutex, attitude_mutex):
+    W, H = 640, 400
+    time.sleep(4) # Give the broadcaster a moment to allocate memory and boot the camera
+
+    # 1. Connect to the shared memory blocks
+    shm_rgb = shared_memory.SharedMemory(name="oak_rgb")
+    shm_gray = shared_memory.SharedMemory(name="oak_gray")
+    shm_attitude = shared_memory.SharedMemory(name="attitude")
+    
+    # 2. Create the NumPy arrays backed by the shared memory
+    shared_rgb = np.ndarray((H, W, 3), dtype=np.uint8, buffer=shm_rgb.buf)
+    shared_gray = np.ndarray((H, W), dtype=np.uint8, buffer=shm_gray.buf)
+    shared_attitude = np.ndarray((3,), dtype=np.float64, buffer=shm_attitude.buf)
+    
+    # 3. Create local variables to hold the copies
+    local_rgb = np.zeros((H, W, 3), dtype=np.uint8)
+    local_gray = np.zeros((H, W), dtype=np.uint8)
+    local_attitude = np.zeros((3,), dtype=np.float64)
+    
+    print("Viewer connected to Shared Memory. Starting display...")
+
+    while True:
+        # 4. Acquire the mutex/lock before copying from the shared memory, and release it immediately after. This is a critical section.
+        with camera_frame_mutex:
+            np.copyto(local_rgb, shared_rgb)
+            np.copyto(local_gray, shared_gray)
+            
+        # 5. Acquire the mutex/lock before copying the attitude data from shared memory, and release it immediately after. This is a critical section.
+        with attitude_mutex:
+            np.copyto(local_attitude, shared_attitude)
+
+        # 6. Print the attitude data to the terminal
+        print(f"Attitude | Roll: {local_attitude[0]:+.4f} | Pitch: {local_attitude[1]:+.4f} | Yaw: {local_attitude[2]:+.4f}")
+
+        # 7. Combine and display the images
+        gray_bgr = cv2.cvtColor(local_gray, cv2.COLOR_GRAY2BGR)
+        combined = np.hstack((local_rgb, gray_bgr))
+        cv2.imshow("RGB and Gray", combined)
+        if cv2.waitKey(60) & 0xFF == ord('q'):
+            break
+    
+    print("Viewer exiting...")
+    cv2.destroyAllWindows()
+    shm_rgb.close()
+    shm_gray.close()
+    shm_attitude.close()
+
+if __name__ == "__main__":
+    # 1. Set the multiprocessing start method to 'spawn' this is so that when a new process is started it
+    #    doesn't inherit the memory of the parent process, and instead creates its own memory space and also
+    #    its own python interpreter. Effectively isolating the processes from each other except for the shared memory.
+    mp.set_start_method('spawn', force=True)
+    W, H = 640, 400
+    RGB_BYTES = W * H * 3
+    GRAY_BYTES = W * H
+    DEPTH_BYTES = W * H * 2 
+    CALIB_BYTES = 3 * 3 * 8 
+    ATTITUDE_BYTES = 3 * 8 # 3 float64 numbers (Roll, Pitch, Yaw = 24 bytes)
+
+    # 2. Create the shared memory for RGB, gray, depth, camera calibration matrix, and attitude
+    print("Broadcaster tester allocating shared memory...")
+    shm_rgb = shared_memory.SharedMemory(create=True, size=RGB_BYTES, name="oak_rgb")
+    shm_gray = shared_memory.SharedMemory(create=True, size=GRAY_BYTES, name="oak_gray")
+    shm_depth = shared_memory.SharedMemory(create=True, size=DEPTH_BYTES, name="oak_depth")
+    shm_calib = shared_memory.SharedMemory(create=True, size=CALIB_BYTES, name="oak_calib")
+    shm_attitude = shared_memory.SharedMemory(create=True, size=ATTITUDE_BYTES, name="attitude")
+    
+    # 3. Initialize the three required locks
+    camera_frame_mutex = mp.Lock()
+    camera_calibration_mutex = mp.Lock()
+    attitude_mutex = mp.Lock()
+
+    # 4. Define the processes
+    broadcaster_process = mp.Process(target=broadcaster, args=(camera_frame_mutex, camera_calibration_mutex, attitude_mutex))
+    viewer_process = mp.Process(target=test_viewer, args=(camera_frame_mutex, attitude_mutex))
+
+    try:
+        # 5. Start the broadcaster and viewer processes
+        broadcaster_process.start()
+        viewer_process.start()
+
+        # 6. Wait until the user presses 'q' to close the viewer window
+        viewer_process.join()
+        
+        # 7. Kill the broadcaster once the viewer is closed
+        broadcaster_process.terminate()
+        broadcaster_process.join()
+        
+    except KeyboardInterrupt:
+        print("\nBroadcaster tester caught keyboard interrupt. Shutting down...")
+    finally:
+        # 8. Clean up the shared memory(no memory leaks)
+        print("Broadcaster tester cleaning up shared memory...")
+        shm_rgb.close()
+        shm_rgb.unlink()
+        shm_gray.close()
+        shm_gray.unlink()
+        shm_depth.close()
+        shm_depth.unlink()
+        shm_calib.close()
+        shm_calib.unlink()
+        shm_attitude.close()
+        shm_attitude.unlink()
+        print("Broadcaster tester processes terminated safely.")
